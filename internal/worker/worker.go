2c52f2ecae353b13632c6e787197e92fd40dfcde
rtsoy <tsoiroman98@gmail.com>
2023-08-14 00:16:45 +0600

    refactor(worker): improve email formatting and logging

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -14,7 +14,7 @@ import (
 )
 
 func Run(ctx context.Context, cfg *config.Config, repository *repository.Repository) error {
-	log.Println("(worker) started")
+	log.Println("worker started")
 
 	for {
 		time.Sleep(time.Second * 5) // Todo: Minute / Hour / Day ?
@@ -25,8 +25,10 @@ func Run(ctx context.Context, cfg *config.Config, repository *repository.Reposit
 			},
 		})
 		if err != nil {
-			log.Fatalf("(worker) failed to retrieve capsules: %s", err)
+			log.Printf("(worker) failed to retrieve capsules: %s\n", err)
 		}
+
+		fmt.Println("expired", expiredCapsules)
 
 		if len(expiredCapsules) == 0 {
 			continue
@@ -41,15 +43,36 @@ func Run(ctx context.Context, cfg *config.Config, repository *repository.Reposit
 				continue
 			}
 
-			if err := sendEmail(cfg, "TODO???", "?", []string{user.Email}); err != nil {
+			// ? Change in the same style as the front-end design ?
+			body := fmt.Sprintf(`
+			<html>
+			<body style="font-family: Arial, sans-serif; background-color: #f7f7f7; margin: 0; padding: 0;">
+			<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%s" style="max-width: 600px; margin: 20px auto; border-collapse: collapse; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);">
+				<tr>
+					<td style="background-color: #000; padding: 40px 20px; text-align: center;">
+						<h1 style="color: #ffffff; font-size: 28px;">💌 Your Time Capsule Has Been Opened</h1>
+					</td>
+				</tr>
+				<tr>
+					<td style="background-color: #ffffff; padding: 40px 40px;">
+						<p style="color: #333333; font-size: 18px; line-height: 1.5;">Dear %s,</p>
+						<p style="color: #333333; font-size: 18px; line-height: 1.5;">We're thrilled to share that the moment you've been waiting for has arrived. Your time capsule has been opened, revealing the cherished memories and heartfelt messages you've kept safe.</p>
+						<p style="color: #333333; font-size: 18px; line-height: 1.5;">Take your time to immerse yourself in the past and relive those beautiful moments. The past is a treasure trove of emotions, and we're honored to be a part of this journey with you.</p>
+						<p style="color: #333333; font-size: 18px; line-height: 1.5;">Thank you for sharing these memories with us. Here's to celebrating the richness of life and the stories that shape us.</p>
+					</td>
+				</tr>
+				</tr>
+			</table>
+			</body>
+			</html>
+            `, "%", user.Username)
+
+			if err := sendEmail(cfg, "Time Capsule Opened!", body, []string{user.Email}); err != nil {
 				log.Println(err) // ? fatal
 				continue
 			}
 
-			if err := repository.DeleteCapsule(ctx, capsule.ID); err != nil {
-				log.Printf("(worker) failed to delete capsule from db with id=%s: %s\n", capsule.ID.Hex(), err) // ? fatal
-				continue
-			}
+			fmt.Println("email sent")
 		}
 	}
 
@@ -66,6 +89,8 @@ func sendEmail(cfg *config.Config, subject, body string, to []string) error {
 
 	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 
+	subject = "Subject:" + subject + "\n"
+
 	if err := smtp.SendMail(
 		fmt.Sprintf("%s:%s", cfg.SMTPHost, cfg.SMTPPort),
 		auth,
@@ -73,7 +98,7 @@ func sendEmail(cfg *config.Config, subject, body string, to []string) error {
 		to,
 		[]byte(subject+mime+body),
 	); err != nil {
-		return fmt.Errorf("(worker) failed to send an email: %s", err)
+		return fmt.Errorf("(worker-email) failed to send an email: %s\n", err)
 	}
 
 	return nil
